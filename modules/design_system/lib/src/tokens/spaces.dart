76d2be9410f75467a837a7704823e1567fe541cb
Alvaro Prado <alvarob.prado@hotmail.com>
2022-09-14 19:08:18 -0400

    feature: add AppSpaces

---
+++ modules/design_system/lib/src/tokens/spaces.dart
@@ -0,0 +1,14 @@
+abstract class AppSpaces {
+  static const _multiplier = 8.0;
+
+  static const spaceXXXS = _multiplier;
+  static const spaceXXS = _multiplier * 2;
+  static const spaceXS = _multiplier * 3;
+  static const spaceS = _multiplier * 4;
+  static const space = _multiplier * 5;
+  static const spaceM = _multiplier * 6;
+  static const spaceL = _multiplier * 7;
+  static const spaceXL = _multiplier * 8;
+  static const spaceXXL = _multiplier * 9;
+  static const spaceXXXL = _multiplier * 10;
+}
