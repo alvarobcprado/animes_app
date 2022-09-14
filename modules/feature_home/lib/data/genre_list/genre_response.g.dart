934117c48230bc8e63b0ea39590a35e7100ee578
rita monteiro <ritaalvesmonteiro@gmail.com>
2022-09-14 19:04:14 -0300

    feat: create genre response object

---
+++ modules/feature_home/lib/data/genre_list/genre_response.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'genre_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+GenreResponse _$GenreResponseFromJson(Map<String, dynamic> json) =>
+    GenreResponse(
+      json['name'] as String?,
+    );
+
+Map<String, dynamic> _$GenreResponseToJson(GenreResponse instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+    };
