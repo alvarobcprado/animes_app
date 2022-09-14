a43923fce3e22a9171d84d2befeeb659c935c6e0
rita monteiro <ritaalvesmonteiro@gmail.com>
2022-09-14 19:00:06 -0300

    feat: create anime aired date response object

---
+++ modules/feature_home/lib/data/anime_list/anime_aired_date_response.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'anime_aired_date_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+AnimeAiredDateResponse _$AnimeAiredDateResponseFromJson(
+        Map<String, dynamic> json) =>
+    AnimeAiredDateResponse(
+      json['release'] as String?,
+    );
+
+Map<String, dynamic> _$AnimeAiredDateResponseToJson(
+        AnimeAiredDateResponse instance) =>
+    <String, dynamic>{
+      'release': instance.from,
+    };
