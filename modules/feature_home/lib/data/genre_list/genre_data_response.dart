6b879d4b36afbe07e2398eed7d4f5196302d013d
rita monteiro <ritaalvesmonteiro@gmail.com>
2022-09-14 19:04:29 -0300

    feat: create genre data response object

---
+++ modules/feature_home/lib/data/genre_list/genre_data_response.dart
@@ -0,0 +1,17 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'genre_response.dart';
+
+part 'genre_data_response.g.dart';
+
+@JsonSerializable(fieldRename: FieldRename.snake)
+class GenreDataResponse {
+  GenreDataResponse(this.data);
+
+  factory GenreDataResponse.fromJson(Map<String, dynamic> json) =>
+      _$GenreDataResponseFromJson(json);
+
+  @JsonKey(name: 'genreList')
+  final List<GenreResponse>? data;
+
+  Map<String, dynamic> toJson() => _$GenreDataResponseToJson(this);
+}
