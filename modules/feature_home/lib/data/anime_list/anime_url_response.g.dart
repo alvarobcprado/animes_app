5dd86a0fff452bed3e7d3f752d36a14107b3e926
rita monteiro <ritaalvesmonteiro@gmail.com>
2022-09-14 19:00:56 -0300

    feat: create anime url response object

---
+++ modules/feature_home/lib/data/anime_list/anime_url_response.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'anime_url_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+AnimeUrlResponse _$AnimeUrlResponseFromJson(Map<String, dynamic> json) =>
+    AnimeUrlResponse(
+      json['next'] as String?,
+    );
+
+Map<String, dynamic> _$AnimeUrlResponseToJson(AnimeUrlResponse instance) =>
+    <String, dynamic>{
+      'next': instance.next,
+    };
