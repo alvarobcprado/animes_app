3c0e1e202c4a35dfb4eafbc3a8fe922787e28f63
Alvaro Prado <alvarob.prado@hotmail.com>
2022-09-14 22:04:03 -0400

    feature: create initial darkAppThemeData

---
+++ modules/design_system/lib/src/foundations/themes/dark_theme.dart
@@ -0,0 +1,10 @@
+import 'package:design_system/src/foundations/colors.dart';
+import 'package:flutter/material.dart';
+
+final darkThemeData = ThemeData(
+  brightness: Brightness.dark,
+  primaryColor: AppColorsFoundation.darkPrimary,
+  backgroundColor: AppColorsFoundation.darkBackground,
+  errorColor: AppColorsFoundation.darkError,
+  useMaterial3: true,
+);
