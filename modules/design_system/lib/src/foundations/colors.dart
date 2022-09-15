c65d7dc89a2a4235e2941f3e080d1741fd306c77
Alvaro Prado <alvarob.prado@hotmail.com>
2022-09-14 22:02:43 -0400

    feature: create initial AppColorsFoundation

---
+++ modules/design_system/lib/src/foundations/colors.dart
@@ -0,0 +1,12 @@
+import 'package:design_system/src/tokens/colors.dart';
+import 'package:flutter/painting.dart';
+
+abstract class AppColorsFoundation {
+  static const Color lightPrimary = AppColors.yInMnBlue;
+  static const Color lightBackground = AppColors.lightBackground;
+  static const Color lightError = AppColors.lightError;
+
+  static const Color darkPrimary = AppColors.babyBlueEyes;
+  static const Color darkBackground = AppColors.darkBackground;
+  static const Color darkError = AppColors.darkError;
+}
