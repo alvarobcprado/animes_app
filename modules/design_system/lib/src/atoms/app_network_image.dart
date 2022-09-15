8537228d5e5f3af7a6bd98bfdf8f8aaa1ce0ce7c
Alvaro Prado <alvarob.prado@hotmail.com>
2022-09-15 19:13:15 -0400

    refact: adjust AppNetworkImage to show loading progress

--- modules/design_system/lib/src/atoms/app_network_image.dart
+++ modules/design_system/lib/src/atoms/app_network_image.dart
@@ -7,12 +7,23 @@ class AppNetworkImage extends StatelessWidget {
     this.width,
     this.height,
     this.fit,
+    this.errorPlaceholder,
+    this.loadingPlaceholder,
   }) : super(key: key);
 
   final String imageUrl;
   final double? width;
   final double? height;
   final BoxFit? fit;
+  final Widget? loadingPlaceholder;
+  final Widget? errorPlaceholder;
+
+  double? loadingProgressOrNull(ImageChunkEvent event) {
+    if (event.expectedTotalBytes != null) {
+      return event.cumulativeBytesLoaded / event.expectedTotalBytes!;
+    }
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,11 +32,23 @@ class AppNetworkImage extends StatelessWidget {
       width: width,
       height: height,
       fit: fit,
+      loadingBuilder: (context, child, loadingProgress) {
+        if (loadingProgress == null) return child;
+
+        final progress = loadingProgressOrNull(loadingProgress);
+
+        return loadingPlaceholder ??
+            Center(
+              child: CircularProgressIndicator(
+                value: progress,
+              ),
+            );
+      },
       errorBuilder: (_, __, ___) => SizedBox(
         width: width,
         height: height,
-        child: const Center(
-          child: Placeholder(),
+        child: Center(
+          child: errorPlaceholder ?? const Placeholder(),
         ),
       ),
     );
