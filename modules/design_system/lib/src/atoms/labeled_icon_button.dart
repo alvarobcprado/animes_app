8615940efe4cd8a6144d10109430e1f0bb51850a
Alvaro Prado <alvarob.prado@hotmail.com>
2022-09-15 19:13:41 -0400

    feature: create initial LabeledIconButton atom

---
+++ modules/design_system/lib/src/atoms/labeled_icon_button.dart
@@ -0,0 +1,58 @@
+import 'package:design_system/src/foundations/sizes.dart';
+import 'package:flutter/material.dart';
+
+class LabeledIconButton extends StatelessWidget {
+  const LabeledIconButton({
+    Key? key,
+    required this.label,
+    required this.icon,
+    this.color,
+    this.isReducedSize = false,
+  }) : super(key: key);
+
+  final String label;
+  final IconData icon;
+  final Color? color;
+  final bool isReducedSize;
+
+  @override
+  Widget build(BuildContext context) {
+    final buttonColor = color ?? Theme.of(context).primaryColor;
+    final buttonTextStyle = Theme.of(context).textTheme.button?.copyWith(
+          color: buttonColor,
+        );
+
+    return SizedBox(
+      width: isReducedSize ? null : double.infinity,
+      child: OutlinedButton(
+        style: OutlinedButton.styleFrom(
+          side: BorderSide(
+            color: buttonColor,
+            width: AppSizesFoundation.buttonBorderStroke,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(
+              AppSizesFoundation.buttonBorderRadius,
+            ),
+          ),
+        ),
+        onPressed: () {},
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              label,
+              style: buttonTextStyle,
+            ),
+            const SizedBox(width: AppSizesFoundation.baseSpace),
+            Icon(
+              icon,
+              color: buttonColor,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
