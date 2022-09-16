aa8c6882cc6dcad8d5e47882dfa8d660e287109c
rita monteiro <ritaalvesmonteiro@gmail.com>
2022-09-16 14:02:40 -0300

    feat: create cache to domain extensions

---
+++ modules/feature_home/lib/data/mapper/cache_to_domain.dart
@@ -0,0 +1,32 @@
+import 'package:data/model/aux/anime_aux_cache.dart';
+import 'package:data/model/aux/anime_details_aux_cache.dart';
+import 'package:data/model/aux/genre_aux_cache.dart';
+
+import '../../domain/anime.dart';
+import '../../domain/anime_details.dart';
+
+extension AnimeListAuxCacheToAnimeList on List<AnimeAuxCache> {
+  List<Anime> toAnimeList() {
+    return map((animeCache) => Anime(
+          animeCache.id,
+          animeCache.image,
+          animeCache.title,
+          animeCache.genres,
+          animeCache.release,
+          animeCache.totalEpisodes,
+        )).toList();
+  }
+}
+
+extension AnimeDetailsAuxCacheToAnimeDetails on AnimeDetailsAuxCache {
+  AnimeDetails toAnimeDetails() {
+    return AnimeDetails(
+        id, title, titleEnglish, image, release, end, synopsis, score);
+  }
+}
+
+extension GenreListAuxCacheToGenreList on List<GenreAuxCache> {
+  List<String> toGenreList() {
+    return map((genreCache) => genreCache.name).toList();
+  }
+}
