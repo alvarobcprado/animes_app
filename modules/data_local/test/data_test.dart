78f22a8886eea14c3c301dab42a8eb1b98e7e0ff
rita monteiro <ritaalvesmonteiro@gmail.com>
2022-09-19 18:22:45 -0300

    feat: replace name data module with data_local in pubspec.yaml from data_local

--- modules/data_local/test/data_test.dart
+++ modules/data_local/test/data_test.dart
@@ -1,6 +1,6 @@
+import 'package:data_local/data.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:data/data.dart';
 
 void main() {
   test('adds one to input values', () {
