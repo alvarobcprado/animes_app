0fd7feeeee2ec7f8348c1cddd443b7c0d3c7e117
Alvaro Prado <alvarob.prado@hotmail.com>
2022-09-19 19:22:46 -0400

    feature: add AppColorsFoundation extension to themeDatas

--- modules/design_system/lib/src/foundations/themes/dark_theme.dart
+++ modules/design_system/lib/src/foundations/themes/dark_theme.dart
@@ -1,10 +1,14 @@
 import 'package:design_system/src/foundations/colors.dart';
+import 'package:design_system/src/tokens/colors.dart';
 import 'package:flutter/material.dart';
 
 final darkAppThemeData = ThemeData(
   brightness: Brightness.dark,
-  primaryColor: AppColorsFoundation.darkPrimary,
-  backgroundColor: AppColorsFoundation.darkBackground,
-  errorColor: AppColorsFoundation.darkError,
+  primaryColor: AppColors.babyBlueEyes,
+  backgroundColor: AppColors.darkBackground,
+  errorColor: AppColors.darkError,
   useMaterial3: true,
+  extensions: const [
+    AppColorsFoundation.dark,
+  ],
 );
