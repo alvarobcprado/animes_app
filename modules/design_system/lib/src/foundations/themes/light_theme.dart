d3afb180d73b4a35f10a268ce5eaff8d8ee9e188
Álvaro Prado <56163128+alvarobcprado@users.noreply.github.com>
2022-09-19 20:51:53 -0400

    Feature/add theme extension colors (#10)
    * feature: change AppColorsFoundation to ThemeExtension approach
    
    * feature: add AppColorsFoundation extension to themeData

--- modules/design_system/lib/src/foundations/themes/light_theme.dart
+++ modules/design_system/lib/src/foundations/themes/light_theme.dart
@@ -1,10 +1,14 @@
 import 'package:design_system/src/foundations/colors.dart';
+import 'package:design_system/src/tokens/colors.dart';
 import 'package:flutter/material.dart';
 
 final lightAppThemeData = ThemeData(
   brightness: Brightness.light,
-  primaryColor: AppColorsFoundation.lightPrimary,
-  backgroundColor: AppColorsFoundation.lightBackground,
-  errorColor: AppColorsFoundation.lightError,
+  primaryColor: AppColors.yInMnBlue,
+  backgroundColor: AppColors.lightBackground,
+  errorColor: AppColors.lightError,
   useMaterial3: true,
+  extensions: const [
+    AppColorsFoundation.light,
+  ],
 );
