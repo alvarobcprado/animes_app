a8f483b779a2e07ab26812a5b542325c7d21b44e
Alvaro Prado <alvarob.prado@hotmail.com>
2022-09-20 21:02:01 -0400

    feature: create home routes basic structure

---
+++ modules/feature_home/lib/src/routes/home_routes.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:core/core.dart' show GoRoute, GoRouter;
+
+/// /home
+const _homeRoute = 'Home';
+
+/// /home/sub
+const _homeSubRoute = 'HomeSubPage';
+
+class HomeRoutes extends GoRoute {
+  HomeRoutes()
+      : super(
+          path: '/home',
+          name: _homeRoute,
+          pageBuilder: (context, state) => MaterialPage(
+            child: Builder(
+              builder: (context) {
+                return Scaffold(
+                  appBar: AppBar(
+                    title: const Text('Home'),
+                    actions: [
+                      IconButton(
+                        icon: const Icon(Icons.arrow_right),
+                        onPressed: () => GoRouter.of(context).goToHomeSubPage(),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+          routes: [
+            GoRoute(
+              path: 'sub',
+              name: _homeSubRoute,
+              pageBuilder: (context, state) => MaterialPage(
+                child: Builder(
+                  builder: (context) {
+                    return Scaffold(
+                      appBar: AppBar(
+                        title: const Text('Home Sub'),
+                        actions: [
+                          IconButton(
+                            icon: const Icon(Icons.arrow_back),
+                            onPressed: () => GoRouter.of(context).pop(),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+        );
+}
+
+extension HomeRoutesExtension on GoRouter {
+  void goToHomeSubPage() {
+    goNamed(_homeSubRoute);
+  }
+}
