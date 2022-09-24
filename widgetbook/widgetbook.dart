42e7e93a5703287ef21354cd8e14a1a1ccbea213
Álvaro Prado <56163128+alvarobcprado@users.noreply.github.com>
2022-09-24 19:47:11 -0400

    Feature/add search bar component (#16)
    * feature: add AppSearchBar component
    
    * feature: add AppSearchBar to widgetbook
    
    * bugfix: adjust main app router

--- widgetbook/widgetbook.dart
+++ widgetbook/widgetbook.dart
@@ -1,13 +1,13 @@
 // ignore_for_file: depend_on_referenced_packages, unused_import
 
+import 'package:design_system/design_system.dart';
 import 'package:flutter/material.dart';
 import 'package:widgetbook/widgetbook.dart';
 
 import 'atoms/app_network_image_component.dart';
 import 'atoms/labeled_chip_component.dart';
 import 'atoms/labeled_icon_button_component.dart';
-import 'package:design_system/design_system.dart';
-
+import 'molecules/app_search_bar_component.dart';
 import 'molecules/filter_select_chip_list.dart';
 
 class WidgetbookHotReload extends StatelessWidget {
@@ -32,6 +32,7 @@ class WidgetbookHotReload extends StatelessWidget {
               name: 'Molecules',
               widgets: [
                 FilterSelectChipListComponent(),
+                AppSearchBarComponent(),
               ],
             ),
           ],
