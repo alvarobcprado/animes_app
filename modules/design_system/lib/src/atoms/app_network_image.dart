7ed3679130f59f1432ccae2afc30c2c4957306fb
Álvaro Prado <56163128+alvarobcprado@users.noreply.github.com>
2022-09-24 19:48:32 -0400

    Feature/add image card button component (#17)
    * bugfix: adjust AppNetworkImage loading indicator
    
    * bugfix: adjust LabeledIconButton properties
    
    * feature: add ImageCardButton component
    
    * feature: add ImageCardButton to widgetbook

--- modules/design_system/lib/src/atoms/app_network_image.dart
+++ modules/design_system/lib/src/atoms/app_network_image.dart
@@ -1,3 +1,4 @@
+import 'package:design_system/src/foundations/colors.dart';
 import 'package:flutter/material.dart';
 
 class AppNetworkImage extends StatelessWidget {
@@ -9,6 +10,7 @@ class AppNetworkImage extends StatelessWidget {
     this.fit,
     this.errorPlaceholder,
     this.loadingPlaceholder,
+    this.loadingColor,
   }) : super(key: key);
 
   final String imageUrl;
@@ -17,32 +19,30 @@ class AppNetworkImage extends StatelessWidget {
   final BoxFit? fit;
   final Widget? loadingPlaceholder;
   final Widget? errorPlaceholder;
-
-  double? loadingProgressOrNull(ImageChunkEvent event) {
-    if (event.expectedTotalBytes != null) {
-      return event.cumulativeBytesLoaded / event.expectedTotalBytes!;
-    }
-    return null;
-  }
+  final Color? loadingColor;
 
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).extension<AppColorsFoundation>()!;
     return Image.network(
       imageUrl,
       width: width,
       height: height,
       fit: fit,
-      loadingBuilder: (context, child, loadingProgress) {
-        if (loadingProgress == null) return child;
-
-        final progress = loadingProgressOrNull(loadingProgress);
-
-        return loadingPlaceholder ??
-            Center(
-              child: CircularProgressIndicator(
-                value: progress,
-              ),
-            );
+      frameBuilder: (context, child, frame, wasSynchronouslyLoaded) {
+        return AnimatedSwitcher(
+          duration: const Duration(
+            milliseconds: 250,
+          ),
+          child: frame != null
+              ? SizedBox.expand(child: child)
+              : Center(
+                  child: loadingPlaceholder ??
+                      CircularProgressIndicator(
+                        color: loadingColor ?? colors.primary,
+                      ),
+                ),
+        );
       },
       errorBuilder: (_, __, ___) => SizedBox(
         width: width,
