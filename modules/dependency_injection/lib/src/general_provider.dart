9eb378c88257a2e2f5de0c4dd34a66af89f2dac0
Alvaro Prado <alvarob.prado@hotmail.com>
2022-10-04 21:00:07 -0400

    feature: create GeneralProvider Widget

---
+++ modules/dependency_injection/lib/src/general_provider.dart
@@ -0,0 +1,21 @@
+import 'package:core/dependencies/dependency_injection.dart';
+import 'package:dependency_injection/src/common/common_providers.dart';
+import 'package:dependency_injection/src/modules/feature_home/feature_home_providers.dart';
+import 'package:flutter/material.dart';
+
+class GeneralProvider extends StatelessWidget {
+  const GeneralProvider({super.key, required this.child});
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ...commonProviders(),
+        ...featureHomeProviders(),
+      ],
+      builder: (context, child) => child!,
+      child: child,
+    );
+  }
+}
