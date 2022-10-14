3fad5103f81324c192fd69acfe0bad3e1ab9cda9
Alvaro Prado <alvarob.prado@hotmail.com>
2022-10-13 21:03:10 -0400

    refact: adjust widgetbook example

--- widgetbook/atoms/labeled_card_component.dart
+++ widgetbook/atoms/labeled_card_component.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: depend_on_referenced_packages
 
+import 'package:design_system/design_system.dart';
 import 'package:flutter/material.dart';
 import 'package:widgetbook/widgetbook.dart';
-import 'package:design_system/design_system.dart';
 
 class LabeledCardComponent extends WidgetbookWidget {
   LabeledCardComponent({Key? key})
@@ -17,15 +17,27 @@ class LabeledCardComponent extends WidgetbookWidget {
                   child: Column(
                     children: [
                       LabeledCard(
-                        firstTitle: 'Nome:',
-                        secondTitle: 'N° de temporadas:',
-                        thirdTitle: 'Data de lançamento:',
-                        fourthTitle: 'Gênero:',
-                        firstSubtitle: 'Default',
-                        secondSubtitle: 'Default',
-                        thirdSubtitle: 'Default',
-                        fourthSubtitle: 'Default',
-                        imageUrl: 'https://picsum.photos/400/600',
+                        item: LabeledCardItem(
+                          imageUrl: 'https://picsum.photos/200/300',
+                          labels: [
+                            LabeledCardText(
+                              title: 'Título',
+                              subtitle: 'Anime de ação',
+                            ),
+                            LabeledCardText(
+                              title: 'Gênero',
+                              subtitle: 'Ação, Aventura, Comédia',
+                            ),
+                            LabeledCardText(
+                              title: 'Episódios',
+                              subtitle: '12',
+                            ),
+                            LabeledCardText(
+                              title: 'Status',
+                              subtitle: 'Em andamento',
+                            ),
+                          ],
+                        ),
                         onTap: () {},
                       ),
                     ],
