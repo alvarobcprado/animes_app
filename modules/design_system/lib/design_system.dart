dfa698b2a786d5faa77703c0b04a54fc8b2b9092
Alvaro Prado <alvarob.prado@hotmail.com>
2022-10-13 21:01:37 -0400

    feature: add design system models export

--- modules/design_system/lib/design_system.dart
+++ modules/design_system/lib/design_system.dart
@@ -1,5 +1,6 @@
 library design_system;
 
-export 'src/foundations/foundations.dart';
 export 'src/atoms/atoms.dart';
+export 'src/foundations/foundations.dart';
+export 'src/models/models.dart';
 export 'src/molecules/molecules.dart';
