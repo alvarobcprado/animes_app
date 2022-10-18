792b8d29c3ec3d3e868ad07c77b2c56f01830648
Alvaro Prado <alvarob.prado@hotmail.com>
2022-10-18 19:14:08 -0400

    feat: add AnimeListPage to feature-home routes

--- modules/feature_home/lib/src/routes/home_routes.dart
+++ modules/feature_home/lib/src/routes/home_routes.dart
@@ -1,62 +1,19 @@
 import 'package:core/dependencies/routing.dart';
+import 'package:feature_home/src/presentation/anime_list/anime_list_page.dart';
 import 'package:flutter/material.dart';
 
-/// /home
 const _homeRoute = 'Home';
-
-/// /home/sub
-const _homeSubRoute = 'HomeSubPage';
 
 class HomeRoutes extends GoRoute {
   HomeRoutes()
       : super(
           path: '/home',
           name: _homeRoute,
-          pageBuilder: (context, state) => MaterialPage(
-            child: Builder(
-              builder: (context) {
-                return Scaffold(
-                  appBar: AppBar(
-                    title: const Text('Home'),
-                    actions: [
-                      IconButton(
-                        icon: const Icon(Icons.arrow_right),
-                        onPressed: () => GoRouter.of(context).goToHomeSubPage(),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
+          pageBuilder: (context, state) => const MaterialPage(
+            child: AnimeListPage(),
           ),
-          routes: [
-            GoRoute(
-              path: 'sub',
-              name: _homeSubRoute,
-              pageBuilder: (context, state) => MaterialPage(
-                child: Builder(
-                  builder: (context) {
-                    return Scaffold(
-                      appBar: AppBar(
-                        title: const Text('Home Sub'),
-                        actions: [
-                          IconButton(
-                            icon: const Icon(Icons.arrow_back),
-                            onPressed: () => GoRouter.of(context).pop(),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ),
-          ],
+          routes: [],
         );
 }
 
-extension HomeRoutesExtension on GoRouter {
-  void goToHomeSubPage() {
-    goNamed(_homeSubRoute);
-  }
-}
+extension HomeRoutesExtension on GoRouter {}
