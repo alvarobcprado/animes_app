51bc839501f3ab24f8c534939a0ed3e37d89a39b
rita monteiro <ritaalvesmonteiro@gmail.com>
2022-10-25 11:31:25 -0300

    feat: create SearchedAnimeListStore

---
+++ modules/feature_home/lib/src/presentation/anime_list/stores/searched_anime_list_store.dart
@@ -0,0 +1,35 @@
+import 'package:feature_home/src/domain/use_cases/get_searched_anime_list_use_case.dart';
+import 'package:flutter_triple/flutter_triple.dart';
+
+import '../../../domain/models/anime.dart';
+
+class SearchedAnimesModel {
+  SearchedAnimesModel({
+    this.animes,
+  });
+
+  List<Anime>? animes;
+}
+
+class SearchedAnimeListStore
+    extends StreamStore<Exception, SearchedAnimesModel> {
+  SearchedAnimeListStore({
+    required GetSearchedAnimeListUseCase getSearchedAnimeListUseCase,
+  })  : _getSearchedAnimeListUseCase = getSearchedAnimeListUseCase,
+        super(SearchedAnimesModel());
+
+  final GetSearchedAnimeListUseCase _getSearchedAnimeListUseCase;
+
+  Future<void> getSearchedAnimes(String query) async {
+    setLoading(true);
+    final animes = await _getSearchedAnimeListUseCase.call(
+        params: GetSearchedAnimeListUseCaseParams(query: query));
+    animes.when(success: (animes) {
+      state.animes = animes;
+      update(state);
+    }, error: (exception) {
+      setError(exception);
+    });
+    setLoading(false);
+  }
+}
