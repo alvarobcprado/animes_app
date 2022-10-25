607755f542cfa368bb00c68bc285f44410f36c22
rita monteiro <ritaalvesmonteiro@gmail.com>
2022-10-25 11:40:07 -0300

    feat: add storesProviders in featureHomeProviders list

--- modules/dependency_injection/lib/src/modules/feature_home/feature_home_providers.dart
+++ modules/dependency_injection/lib/src/modules/feature_home/feature_home_providers.dart
@@ -2,6 +2,7 @@ import 'package:core/dependencies/dependency_injection.dart';
 import 'package:dependency_injection/src/modules/feature_home/data_source_providers.dart';
 import 'package:dependency_injection/src/modules/feature_home/network_provider.dart';
 import 'package:dependency_injection/src/modules/feature_home/repositories_providers.dart';
+import 'package:dependency_injection/src/modules/feature_home/stores_providers.dart';
 import 'package:dependency_injection/src/modules/feature_home/use_case_providers.dart';
 
 List<SingleChildWidget> featureHomeProviders() => [
@@ -9,4 +10,5 @@ List<SingleChildWidget> featureHomeProviders() => [
       ...dataSourceProviders(),
       ...repositoryProviders(),
       ...usecaseProviders(),
+      ...storesProviders(),
     ];
