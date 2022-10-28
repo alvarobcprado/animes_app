8c959982da23fbefdfefd6e37db7916f3a3bc058
Alvaro Prado <alvarob.prado@hotmail.com>
2022-10-27 21:47:34 -0400

    refact: change pagination model

--- modules/feature_home/lib/src/data/remote/models/anime_list/anime_data_response.dart
+++ modules/feature_home/lib/src/data/remote/models/anime_list/anime_data_response.dart
@@ -1,21 +1,21 @@
 import 'package:core/dependencies/code_gen.dart';
 
+import 'anime_pagination_response.dart';
 import 'anime_response.dart';
-import 'anime_url_response.dart';
 
 part 'anime_data_response.g.dart';
 
 @JsonSerializable(fieldRename: FieldRename.snake)
 class AnimeDataResponse {
-  AnimeDataResponse(this.animes, this.url);
+  AnimeDataResponse(this.animes, this.pagination);
 
   factory AnimeDataResponse.fromJson(Map<String, dynamic> json) =>
       _$AnimeDataResponseFromJson(json);
 
   @JsonKey(name: 'data')
   final List<AnimeResponse>? animes;
-  @JsonKey(name: 'links')
-  final AnimeUrlResponse? url;
+  @JsonKey(name: 'pagination')
+  final AnimePaginationResponse? pagination;
 
   Map<String, dynamic> toJson() => _$AnimeDataResponseToJson(this);
 }
---
+++ modules/feature_home/lib/src/data/remote/models/anime_list/anime_pagination_response.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'anime_pagination_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+AnimePaginationResponse _$AnimePaginationResponseFromJson(
+        Map<String, dynamic> json) =>
+    AnimePaginationResponse(
+      json['has_next_page'] as bool?,
+    );
+
+Map<String, dynamic> _$AnimePaginationResponseToJson(
+        AnimePaginationResponse instance) =>
+    <String, dynamic>{
+      'has_next_page': instance.hasNextPage,
+    };
