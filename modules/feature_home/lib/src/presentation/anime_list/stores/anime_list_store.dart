03f498eb270c0c8680ba800d612771c1a07ee004
Álvaro Prado <alvaroprado@tokenlab.com.br>
2022-10-31 15:09:31 -0400

    feat: Add genresFilter list to control getAnimeByGenre

--- modules/feature_home/lib/src/presentation/anime_list/stores/anime_list_store.dart
+++ modules/feature_home/lib/src/presentation/anime_list/stores/anime_list_store.dart
@@ -25,9 +25,10 @@ class AnimeListStore extends StreamStore<Exception, AnimesModel> {
   final GetSearchedAnimeListUseCase _getSearchedAnimeListUseCase;
   final GetAnimesByGenreUseCase _getAnimesByGenreUseCase;
 
-  List<Anime> animesByPagination = [];
-  List<Anime> animesByGenre = [];
-  List<Anime> animesBySearch = [];
+  final List<Anime> _animesByPagination = [];
+  final List<Anime> _animesByGenre = [];
+  final List<Anime> _animesBySearch = [];
+  final List<String> _genresFilter = [];
 
   Future<void> getAnimeList() async {
     if (state.animes.isEmpty) {
@@ -38,38 +39,80 @@ class AnimeListStore extends StreamStore<Exception, AnimesModel> {
     }
 
     final result = await _getAnimeListUseCase.call(params: null);
-    result.when(success: (animes) {
-      animesByPagination.addAll(animes);
-      update(AnimesModel(animes: animesByPagination));
-    }, error: (exception) {
-      setError(exception);
-    });
+
+    result.when(
+      success: (animes) {
+        _animesByPagination.addAll(animes);
+        update(AnimesModel(animes: _animesByPagination));
+      },
+      error: (exception) {
+        setError(exception);
+      },
+    );
+
     setLoading(false);
   }
 
   Future<void> getAnimesBySearch(String query) async {
     setLoading(true);
+
     final result = await _getSearchedAnimeListUseCase.call(
-        params: GetSearchedAnimeListUseCaseParams(query: query));
-    result.when(success: (animes) {
-      animesBySearch.addAll(animes);
-      update(AnimesModel(animes: animes));
-    }, error: (exception) {
-      setError(exception);
-    });
+      params: GetSearchedAnimeListUseCaseParams(query: query),
+    );
+
+    result.when(
+      success: (animes) {
+        _animesBySearch.addAll(animes);
+        update(AnimesModel(animes: animes));
+      },
+      error: (exception) {
+        setError(exception);
+      },
+    );
+
     setLoading(false);
   }
 
-  Future<void> getAnimesByGenre(String id) async {
+  Future<void> getAnimesByGenre(String id, bool isAddingGenre) async {
     setLoading(true);
-    final result = await _getAnimesByGenreUseCase.call(
-        params: GetAnimesByGenreUseCaseParams(id: id));
-    result.when(success: (animes) {
-      animesByGenre.insertAll(0, animes);
-      update(AnimesModel(animes: animesByGenre));
-    }, error: (exception) {
-      setError(exception);
-    });
-    setLoading(false);
+    _updateGenresFilter(id, isAddingGenre);
+
+    execute(
+      delay: const Duration(milliseconds: 500),
+      () async {
+        if (_genresFilter.isEmpty) {
+          return AnimesModel(animes: _animesByPagination);
+        }
+
+        final composedGenres = _genresFilter.join(',');
+
+        final result = await _getAnimesByGenreUseCase.call(
+          params: GetAnimesByGenreUseCaseParams(id: composedGenres),
+        );
+
+        return result.when(
+          success: (animes) {
+            _animesByGenre.addAll(animes);
+            return AnimesModel(animes: _animesByGenre);
+          },
+          error: (exception) {
+            throw exception;
+          },
+        );
+      },
+    );
+  }
+
+  void _updateGenresFilter(String id, bool isAddingGenre) {
+    final genresLength = _genresFilter.length;
+    if (isAddingGenre) {
+      _genresFilter.add(id);
+    } else {
+      _genresFilter.remove(id);
+    }
+
+    if (genresLength != _genresFilter.length) {
+      _animesByGenre.clear();
+    }
   }
 }
