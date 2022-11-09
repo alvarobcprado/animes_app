6e2e6e5d7a860b2cc5f95567bd322939f6ceb113
rita monteiro <ritaalvesmonteiro@gmail.com>
2022-11-09 19:27:53 -0300

    feat: generated CoreStrings

---
+++ modules/core/lib/generated/core_strings_en.dart
@@ -0,0 +1,12 @@
+import 'core_strings.dart';
+
+/// The translations for English (`en`).
+class CoreStringsEn extends CoreStrings {
+  CoreStringsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get occurredError => 'An occurred error';
+
+  @override
+  String get connectionFail => 'Connection fail';
+}
