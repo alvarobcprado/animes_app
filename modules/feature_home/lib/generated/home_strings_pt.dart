5cf844c53ee36f7dd44c936899e5f4da7352d930
rita monteiro <ritaalvesmonteiro@gmail.com>
2022-11-11 10:50:21 -0300

    feat: add strings

--- modules/feature_home/lib/generated/home_strings_pt.dart
+++ modules/feature_home/lib/generated/home_strings_pt.dart
@@ -4,5 +4,24 @@ import 'home_strings.dart';
 class HomeStringsPt extends HomeStrings {
   HomeStringsPt([String locale = 'pt']) : super(locale);
 
+  @override
+  String get animeDetailsPageOriginalTitle => 'Título original:';
 
+  @override
+  String get animeDetailsPageEnglishTitle => 'Título inglês:';
+
+  @override
+  String get animeDetailsPageRelease => 'Data de lançamento:';
+
+  @override
+  String get animeDetailsPageEnd => 'Data de encerramento:';
+
+  @override
+  String get animeDetailsPageNote => 'Nota:';
+
+  @override
+  String get animeDetailsPageSynopsis => 'Sinopse:';
+
+  @override
+  String get animeDetailsPageFavorite => 'Favoritar';
 }
