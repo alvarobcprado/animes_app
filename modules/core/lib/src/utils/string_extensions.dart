3e6a6b2c839eb0498373e96c014460e7e537b5cc
rita monteiro <ritaalvesmonteiro@gmail.com>
2022-11-11 11:37:03 -0300

    feat: handle error in extension convertDateToCurrentLocale

--- modules/core/lib/src/utils/string_extensions.dart
+++ modules/core/lib/src/utils/string_extensions.dart
@@ -3,12 +3,16 @@ import 'package:intl/intl.dart';
 
 extension StringExtensions on String {
   String convertDateToCurrentLocale(BuildContext context) {
-    final currentLocal = Localizations.localeOf(context);
+    try {
+      final currentLocal = Localizations.localeOf(context);
 
-    final dateFormat =
-        currentLocal == const Locale('pt') ? 'dd/MM/yyyy' : 'MM/dd/yyyy';
-    final dateTime = DateTime.parse(this);
+      final dateFormat =
+          currentLocal == const Locale('pt') ? 'dd/MM/yyyy' : 'MM/dd/yyyy';
+      final dateTime = DateTime.parse(this);
 
-    return DateFormat(dateFormat).format(dateTime);
+      return DateFormat(dateFormat).format(dateTime);
+    } catch (e) {
+      return '';
+    }
   }
 }
