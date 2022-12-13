5f16733f8c035b148c4acf061427c334632fbf6d
Alvaro Prado <alvarob.prado@hotmail.com>
2022-12-13 19:43:33 -0400

    refact: split FavoriteAnimesPage into small widgets

--- modules/feature_home/lib/src/presentation/favorite_animes/favorite_animes_page.dart
+++ modules/feature_home/lib/src/presentation/favorite_animes/favorite_animes_page.dart
@@ -1,12 +1,11 @@
 import 'package:core/core.dart';
 import 'package:core/dependencies/dependency_injection.dart';
-import 'package:core/dependencies/routing.dart';
 import 'package:core/dependencies/state_management.dart';
 import 'package:core/dependencies/utils.dart';
 import 'package:design_system/design_system.dart';
 import 'package:feature_home/generated/home_strings.dart';
 import 'package:feature_home/src/presentation/favorite_animes/stores/stores.dart';
-import 'package:feature_home/src/routes/home_routes.dart';
+import 'package:feature_home/src/presentation/favorite_animes/widgets/favorite_anime_list.dart';
 import 'package:flutter/material.dart';
 
 import 'favorite_animes_controller.dart';
@@ -62,67 +61,36 @@ class _FavoriteAnimesPageState extends State<FavoriteAnimesPage> {
           leftFactor: 2,
           rightFactor: 2,
           child: ScopedBuilder<FavoriteAnimesStore, Exception,
-                  FavoriteAnimesModel>(
-              store: _pageController.favoriteAnimesStore,
-              onLoading: (_) => Center(
-                    child: CircularProgressIndicator(
-                      color: colors.primary,
-                    ),
+              FavoriteAnimesModel>(
+            store: _pageController.favoriteAnimesStore,
+            onLoading: (_) => Center(
+              child: CircularProgressIndicator(
+                color: colors.primary,
+              ),
+            ),
+            onError: (_, error) {
+              final message = error!.getErrorMessage(context);
+              return Center(
+                child: Failure(
+                  message: message,
+                  buttonText: CoreStrings.of(context)!.tryAgain,
+                  onButtonPressed: () => _pageController.getFavoriteAnimes(),
+                ),
+              );
+            },
+            onState: (_, state) {
+              final favorites = state.animes;
+              return Visibility(
+                visible: favorites.isNotEmpty,
+                replacement: Center(
+                  child: Text(
+                    HomeStrings.of(context)!.favoriteAnimesPageNoAnimes,
                   ),
-              onError: (_, error) {
-                final message = error!.getErrorMessage(context);
-                return Center(
-                  child: Failure(
-                      message: message,
-                      buttonText: CoreStrings.of(context)!.tryAgain,
-                      onButtonPressed: () =>
-                          _pageController.getFavoriteAnimes()),
-                );
-              },
-              onState: (_, state) {
-                final favorites = state.animes;
-                return favorites.isNotEmpty
-                    ? CardList(
-                        items: favorites
-                            .map(
-                              (e) => LabeledCardItem(
-                                imageUrl: e.image,
-                                labels: [
-                                  LabeledCardText(
-                                    title: HomeStrings.of(context)!
-                                        .animeListPageLabeledCardTitle,
-                                    subtitle: e.title,
-                                  ),
-                                  LabeledCardText(
-                                    title: HomeStrings.of(context)!
-                                        .animeReleaseDate,
-                                    subtitle: e.release.isNotEmpty
-                                        ? e.release
-                                            .convertDateToCurrentLocale(context)
-                                        : HomeStrings.of(context)!
-                                            .animeInformationSoon,
-                                  ),
-                                  LabeledCardText(
-                                    title:
-                                        HomeStrings.of(context)!.animeEndDate,
-                                    subtitle: e.end.isNotEmpty
-                                        ? e.end
-                                            .convertDateToCurrentLocale(context)
-                                        : HomeStrings.of(context)!
-                                            .animeInformationSoon,
-                                  ),
-                                ],
-                              ),
-                            )
-                            .toList(),
-                        onTap: (index) => GoRouter.of(context).pushDetails(
-                          state.animes[index].id,
-                        ),
-                      )
-                    : Center(
-                        child: Text(HomeStrings.of(context)!.favoriteAnimesPageNoAnimes),
-                      );
-              }),
+                ),
+                child: FavoriteAnimeList(favorites: favorites),
+              );
+            },
+          ),
         ),
       ),
     );
---
+++ modules/feature_home/lib/src/presentation/favorite_animes/widgets/favorite_anime_list.dart
@@ -0,0 +1,50 @@
+import 'package:core/core.dart';
+import 'package:core/dependencies/routing.dart';
+import 'package:design_system/design_system.dart';
+import 'package:feature_home/generated/home_strings.dart';
+import 'package:feature_home/src/domain/models/anime_details.dart';
+import 'package:feature_home/src/routes/home_routes.dart';
+import 'package:flutter/material.dart';
+
+class FavoriteAnimeList extends StatelessWidget {
+  const FavoriteAnimeList({
+    Key? key,
+    required this.favorites,
+  }) : super(key: key);
+
+  final List<AnimeDetails> favorites;
+
+  @override
+  Widget build(BuildContext context) {
+    return CardList(
+      items: favorites
+          .map(
+            (e) => LabeledCardItem(
+              imageUrl: e.image,
+              labels: [
+                LabeledCardText(
+                  title: HomeStrings.of(context)!.animeListPageLabeledCardTitle,
+                  subtitle: e.title,
+                ),
+                LabeledCardText(
+                  title: HomeStrings.of(context)!.animeReleaseDate,
+                  subtitle: e.release.isNotEmpty
+                      ? e.release.convertDateToCurrentLocale(context)
+                      : HomeStrings.of(context)!.animeInformationSoon,
+                ),
+                LabeledCardText(
+                  title: HomeStrings.of(context)!.animeEndDate,
+                  subtitle: e.end.isNotEmpty
+                      ? e.end.convertDateToCurrentLocale(context)
+                      : HomeStrings.of(context)!.animeInformationSoon,
+                ),
+              ],
+            ),
+          )
+          .toList(),
+      onTap: (index) => GoRouter.of(context).pushDetails(
+        favorites[index].id,
+      ),
+    );
+  }
+}
