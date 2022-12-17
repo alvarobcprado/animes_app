3f5a84bc45d46488fb74dbdfefe8dfd02f2b98f5
Alvaro Prado <alvarob.prado@hotmail.com>
2022-12-17 15:08:45 -0400

    feat: create RouteNames class

--- modules/core/lib/dependencies/routing.dart
+++ modules/core/lib/dependencies/routing.dart
@@ -1,3 +1,14 @@
 library routing;
 
 export 'package:go_router/go_router.dart';
+
+/// A class to centralize all route names in core module to avoid duplication
+/// and allow navigation between modules.
+/// Example: An auth module can navigate to home module by using
+///
+/// `GoRouter.of(context).goNamed(RouteNames.home)`
+class RouteNames {
+  static const String home = 'Home';
+  static const String details = 'Details';
+  static const String favorite = 'Favorite';
+}
