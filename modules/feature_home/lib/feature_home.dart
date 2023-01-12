3128ad5f7915c7277a9792455ba07b1f2f418981
Alvaro Prado <alvarob.prado@hotmail.com>
2023-01-11 21:22:55 -0400

    refact: remove unnecessary feature_home export

--- modules/feature_home/lib/feature_home.dart
+++ modules/feature_home/lib/feature_home.dart
@@ -1,11 +1,10 @@
 library feature_home;
 
+export 'src/boundary/boundary.dart';
 export 'src/data/data.dart';
 export 'src/domain/models/models.dart';
 export 'src/domain/repositories/repositories.dart';
 export 'src/domain/use_cases/use_cases.dart';
-export 'src/presentation/anime_details/stores/stores.dart';
 export 'src/presentation/anime_list/stores/stores.dart';
 export 'src/presentation/favorite_animes/stores/stores.dart';
 export 'src/routes/routes.dart';
-export 'src/boundary/boundary.dart';
